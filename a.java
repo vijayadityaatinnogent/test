dd6b7b72ee5b65580fef1ee649b98803b0d144fc
missydodia02 <missy.dodia@innogent.in>
2025-12-24 19:18:58 +0530

    Revert "Devlopment"

--- a.java
+++ a.java
@@ -1,10 +1,6 @@
 public class a {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Missy");
-        
-        System.out.println("hello coder badal form this side");
-        System.out.println("Hello Vijay");
 
     }
 }
